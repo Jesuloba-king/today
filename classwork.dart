9ea0681d295d369b1c2af49a2474db626449dafd
Jesuloba_king <ayojesukunumi1@gmail.com>
2022-07-18 15:13:50 +0100

    class work

---
+++ classwork.dart
@@ -0,0 +1,16 @@
+import "dart:io";
+
+class MyName{
+  void name (){
+    print ("My name is Jesuloba David");
+  }
+
+//Area of a circle is A=pir^2
+  void main(){
+    Area = findArea(a);
+    PI = 3.142;
+    int r = 5;
+    int b = PI*r*r;
+    print('The area of the circle: is $Area ');
+
+  }
